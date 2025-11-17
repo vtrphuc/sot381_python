1dd15ae201b51cf828334c8e133771b796ac8249
vtrphuc <phuc.vtr.67khmt@ntu.edu.vn>
2025-11-17 07:31:29 +0700

    Create chuvidientichhcn.py

---
+++ chuvidientichhcn.py
@@ -0,0 +1,9 @@
+#chu vi hinh chu nhat
+
+dai = float(input("nhap chieu dai: "))
+rong = float(input("nhap chieu rong: "))
+chu_vi = (dai + rong) * 2
+dien_tich = dai * rong
+
+print("chu vi hinh chu nhat la:", chu_vi)
+print("dien tich hinh chu nhat la:", dien_tich)
