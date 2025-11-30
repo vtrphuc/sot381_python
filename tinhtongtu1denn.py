90ffe45a8ec14a459175732808cbf87eb26965f9
vtrphuc <phuc.vtr.67khmt@ntu.edu.vn>
2025-11-30 16:59:08 +0700

    Delete tinhtongtu1denn.py

--- tinhtongtu1denn.py
+++
@@ -1,7 +0,0 @@
-#tính tổng từ 1 -> n
-n = int( input("nhập số n: ") )
-tong = 0
-for i in range(1, n+1):
-    tong = tong + i
-    print(f"+ thêm {i} -> tổng: {tong}")
-print(f"kết quả: 1 + 2 + ... + {n} = {tong}")
