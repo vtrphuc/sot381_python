22ca0390f2af30ae934f02bfadd7a25e163ad4bc
vtrphuc <phuc.vtr.67khmt@ntu.edu.vn>
2025-11-30 16:59:06 +0700

    Delete tinhchisoBMI.py

--- tinhchisoBMI.py
+++
@@ -1,6 +0,0 @@
-#tinh chi so BMI
-
-can_nang = float(input("nhap can nang: "))
-chieu_cao = float(input("nhap chieu cao: "))
-chi_so_BMI = can_nang / (chieu_cao * chieu_cao)
-print("chi so BMI la:", chi_so_BMI)
