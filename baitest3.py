8b16457eca834743b6dfc88885dcd2ffcb54341f
vtrphuc <phuc.vtr.67khmt@ntu.edu.vn>
2025-12-08 15:49:44 +0700

    Update baitest3.py

--- baitest3.py
+++ baitest3.py
@@ -1,5 +1,7 @@
 for i in range(2, 10):
     print(f'bảng cửu chương số {i}')
-    for j in range(1, 11):
+    j = 0
+    while j < 10:
+        j += 1
         kq = i * j
         print(f'{i} x {j} = {kq}')
