910139ecd6095431ed43c9cf69604017ad69bfb2
vtrphuc <phuc.vtr.67khmt@ntu.edu.vn>
2025-12-22 09:04:00 +0700

    Create btth_bt1.py

---
+++ string/btth_bt1.py
@@ -0,0 +1,5 @@
+def xinchao(ten, tuoi):
+    return f'xin chào {ten}, năm nay bạn {tuoi} tuổi'
+ten = input('hãy nhập tên của bạn: ')
+tuoi = input('hãy nhập tuổi của bạn: ')
+print(xinchao(ten, tuoi))
